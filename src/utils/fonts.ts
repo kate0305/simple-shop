84e04ce516b4e5b3c5e3c5304c8dac692777fcd6
Katsiaryna Voronich <katarinka403@gmail.com>
2024-07-25 02:38:11 +0300

    feat: add utils

---
+++ src/utils/fonts.ts
@@ -0,0 +1,13 @@
+import { Inter, Caveat } from 'next/font/google';
+
+export const inter = Inter({
+  weight: ['400', '500', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+});
+
+export const caveat = Caveat({
+  weight: ['700'],
+  subsets: ['latin'],
+  display: 'swap',
+});
